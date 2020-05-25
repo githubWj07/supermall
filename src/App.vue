ecff90b404c9dcd34156f0e82110fbe040c27928
AmberQu <1037313052@qq.com>
2020-05-25 15:02:37 +0800

    'tabbar'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <div id="app">
     <tab-bar>
-		<tab-bar-item></tab-bar-item>
-		<tab-bar-item></tab-bar-item>
-		<tab-bar-item></tab-bar-item>
-		<tab-bar-item></tab-bar-item>
+		<tab-bar-item>
+			<template #item-icon>
+				<img src="./assets/images/tabbar/icon01.png" alt="">
+			</template>
+			<template #item-text>
+				<div>主页</div>
+			</template>
+		</tab-bar-item>
+		<tab-bar-item>
+			<template #item-icon>
+				<img src="./assets/images/tabbar/icon02.png" alt="">
+			</template>
+			<template #item-text>
+				<div>分类</div>
+			</template>
+		</tab-bar-item>
+		<tab-bar-item>
+			<template #item-icon>
+				<img src="./assets/images/tabbar/icon03.png" alt="">
+			</template>
+			<template #item-text>
+				<div>购物车</div>
+			</template>
+		</tab-bar-item>
+		<tab-bar-item>
+			<template #item-icon>
+				<img src="./assets/images/tabbar/icon04.png" alt="">
+			</template>
+			<template #item-text>
+				<div>我的</div>
+			</template>
+		</tab-bar-item>
 	</tab-bar>
   </div>
 </template>
--- src/components/tabbar/TabBar.vue
+++ src/components/tabbar/TabBar.vue
@@ -1,12 +1,24 @@
 <template>
-	<div>
+	<div class="tab-bar-wrap">
 		<slot></slot>
 	</div>
 </template>
 
 <script>
+	export default {
+		name: 'TabBar'
+	}
 </script>
 
-<style lang="less">
-	
+<style lang="less" scoped>
+	.tab-bar-wrap {
+		font-size: 12px;
+		height: 49px;
+		display: flex;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		box-shadow: 2px 2px 10px  #ececec;
+	}
 </style>
