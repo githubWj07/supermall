dfff8f08927537615fd9759d2ed372d87ce820cc
AmberQu <1037313052@qq.com>
2020-05-28 13:52:24 +0800

    '配置vue.config(设置文件别名)'

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'assets': '@/assets',
+				'components': '@/components',
+				'network': '@/network',
+				'views': '@/views',
+			}
+		}
+	}
+}
