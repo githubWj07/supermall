ddd74ad110f43bc97cd0dd87a91ebd60532b3bb0
AmberQu <1037313052@qq.com>
2020-06-17 10:50:39 +0800

    '更新代码'

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export function request(config){
 	const instance = axios.create({
 		baseURL: 'http://123.207.32.32:8000',
-	
 		timeout: 5000
 	})
 	return instance(config)
