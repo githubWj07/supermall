50ce9de5e6470bb18e3cfd636d538798cae7b550
AmberQu <1037313052@qq.com>
2020-06-18 17:15:48 +0800

    '商品详情页'

--- src/network/detail.js
+++ src/network/detail.js
@@ -10,6 +10,7 @@ export function getDetail(iid){
 	})
 }
 
+//商品信息
 export class Goods {
 	constructor(itemInfo, columns, shopInfo) {
 	    this.title = itemInfo.title;
@@ -21,6 +22,7 @@ export class Goods {
 	}
 }
 
+//商铺信息
 export class Shop {
 	constructor(shopInfo) {
 	    this.shopLogo = shopInfo.shopLogo;
@@ -28,5 +30,13 @@ export class Shop {
 		this.score = shopInfo.score;
 		this.cSells = shopInfo.cSells;
 		this.cGoods = shopInfo.cGoods;
+	}
+}
+
+//参数信息
+export class Param {
+	constructor(info, rule) {
+	    this.info = info.set;
+		this.rule = rule.tables;
 	}
 }
--- src/network/request.js
+++ src/network/request.js
@@ -3,10 +3,6 @@ import axios from 'axios'
 export function request(config){
 	const instance = axios.create({
 		baseURL: 'http://123.207.32.32:8000',
-<<<<<<< HEAD
-=======
-		
->>>>>>> 5f5d61766836f5110b0672b0e52cd371ecf3efcb
 		timeout: 5000
 	})
 	return instance(config)
