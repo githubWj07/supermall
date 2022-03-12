9f08fa3ec109073dd1a170ed4de181bdc1063604
twitzz <2351061127@qq.com.com>
2022-03-12 23:20:03 +0800

    fix:code format

--- src/common/untils.js
+++ src/common/untils.js
@@ -1,45 +1,37 @@
 export function formatDate(date, fmt) {
-
   if (/(y+)/.test(fmt)) {
-
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
-
+    fmt = fmt.replace(
+      RegExp.$1,
+      (date.getFullYear() + "").substr(4 - RegExp.$1.length)
+    );
   }
 
   let o = {
+    "M+": date.getMonth() + 1,
 
-    'M+': date.getMonth() + 1,
+    "d+": date.getDate(),
 
-    'd+': date.getDate(),
+    "h+": date.getHours(),
 
-    'h+': date.getHours(),
+    "m+": date.getMinutes(),
 
-    'm+': date.getMinutes(),
-
-    's+': date.getSeconds()
-
+    "s+": date.getSeconds(),
   };
 
   for (let k in o) {
-
     if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + "";
 
-      let str = o[k] + '';
-
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
-
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? str : padLeftZero(str)
+      );
     }
-
   }
 
   return fmt;
+}
 
-};
-
-
-
-function padLeftZero (str) {
-
-  return ('00' + str).substr(str.length);
-
-};
+function padLeftZero(str) {
+  return ("00" + str).substr(str.length);
+}
--- src/network/detail.js
+++ src/network/detail.js
@@ -1,52 +1,50 @@
-import {request} from './request'
+import { request } from "./request";
 
 //请求详情
-export function getDetail(iid){
-	return request({
-		url:'/detail',
-		params: {
-			iid
-		}
-	})
+export function getDetail(iid) {
+  return request({
+    url: "/detail",
+    params: {
+      iid,
+    },
+  });
 }
 
 //请求推荐recommend
-export function getRecommend(){
-	return request({
-		url:'/recommend'
-	})
+export function getRecommend() {
+  return request({
+    url: "/recommend",
+  });
 }
 
 //商品基本信息
 export class Goods {
-	constructor(itemInfo, columns, shopInfo) {
-	    this.title = itemInfo.title;
-		this.price = itemInfo.price;
-		this.oldPrice = itemInfo.oldPrice;
-		this.lowNowPrice = itemInfo.lowNowPrice;
-		this.discountDesc = itemInfo.discountDesc;
-		this.columns = columns;
-		this.services = shopInfo.services;
-	}
+  constructor(itemInfo, columns, shopInfo) {
+    this.title = itemInfo.title;
+    this.price = itemInfo.price;
+    this.oldPrice = itemInfo.oldPrice;
+    this.lowNowPrice = itemInfo.lowNowPrice;
+    this.discountDesc = itemInfo.discountDesc;
+    this.columns = columns;
+    this.services = shopInfo.services;
+  }
 }
 
 //商铺信息
 export class Shop {
-	constructor(shopInfo) {
-	    this.shopLogo = shopInfo.shopLogo;
-		this.name = shopInfo.name;
-		this.score = shopInfo.score;
-		this.cSells = shopInfo.cSells;
-		this.cGoods = shopInfo.cGoods;
-	}
+  constructor(shopInfo) {
+    this.shopLogo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.score = shopInfo.score;
+    this.cSells = shopInfo.cSells;
+    this.cGoods = shopInfo.cGoods;
+  }
 }
 
 //参数信息
 export class Param {
-	constructor(info, rule) {
-	    this.info = info.set;
-		this.rule = rule.tables;
-	}
+  constructor(info, rule) {
+    this.info = info.set;
+    this.rule = rule.tables;
+  }
 }
-
-
